bac81ced8dcb08b89da693443a9171be9dc6bb9d
Symon He <symonhe@users.noreply.github.com>
2015-01-07 21:59:58 -0800

    OOP BlackJack attempt #2 after Brandon feedback

--- OOP_BlackJack1.rb
+++ OOP_BlackJack1.rb
@@ -1,30 +1,9 @@
-FACES_VALUES = {"1" => 1, "2" =>2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, \
-"8" => 8, "9" => 9, "10" => 10, "J" => 10, "Q" => 10, "K" => 10}
-
-SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
-
-# def num_is_123?(num)
-#   num == 1 || num == 2 || num == 3
-# end
-
-# def say(string)
-#   puts "==> #{string}"
-# end
-
-def num_is_mult_of_10?(num)
-  num % 10  == 0
-end
-
-def num_at_least_100?(num)
-  num > 99
-end
-
 class Card
   attr_reader :face_value, :suit
 
-  def initialize(fv, s)
-    @face_value = fv
-    @suit = s
+  def initialize(facevalue, suitvalue)
+    @face_value = facevalue
+    @suit = suitvalue
   end
 
   def to_s
@@ -36,13 +15,17 @@ end
 class Deck
   attr_accessor :cards
 
+  FACES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+  SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
   def initialize
     @cards = []
-    @faces = FACES_VALUES.keys
+    @faces = FACES
 
     SUITS.each do |suit|
-      @faces.each do |fv|
-        @cards << Card.new(fv, suit)
+      @faces.each do |facevalue|
+        @cards << Card.new(facevalue, suit)
       end
     end
     shuffle_deck
@@ -63,13 +46,15 @@ class Deck
 end
 
 module Hand
+  FACE_VALUES = {"1" => 1, "2" =>2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, \
+  "8" => 8, "9" => 9, "10" => 10, "J" => 10, "Q" => 10, "K" => 10}
 
   def show_hand
     puts "**** #{name}'s Hand ****"
     cards.each do |card|
       puts "=> #{card}"
     end
-    puts "=> Count is: #{get_count[0]} #{:or if !get_count[1].nil?} #{get_count[1] if !(get_count[1].nil?)}"
+    puts "=> Count is: #{get_hand_value[0]} #{:or if !get_hand_value[1].nil?} #{get_hand_value[1] if !(get_hand_value[1].nil?)}"
     puts
   end
 
@@ -80,12 +65,12 @@ module Hand
     puts
   end
 
-  def get_count
+  def get_hand_value
     sum = [0]
     values = []
 
     cards.each do |card|
-      sum[0] += FACES_VALUES[card.face_value]
+      sum[0] += FACE_VALUES[card.face_value]
       values.push(card.face_value)
     end
 
@@ -105,11 +90,11 @@ module Hand
   end
 
   def is_busted?
-    get_count.first > 21
+    get_hand_value.first > 21
   end
 
   def is_natural_21?
-    !get_count[1].nil? && get_count[1] == 21 && cards.count == 2
+    !get_hand_value[1].nil? && get_hand_value[1] == 21 && cards.count == 2
   end
 
 end
@@ -144,13 +129,17 @@ class Blackjack
     @deck = Deck.new()
   end
 
-  # #do I need this?
-  # def print_table
-  # end
+  def number_is_multiple_of_10?(num)
+    num % 10  == 0
+  end
+
+  def number_at_least_100?(num)
+    num > 99
+  end
 
   def get_user_name
     puts "What is your name?"
-    gets.chomp().capitalize
+    gets.chomp.capitalize
   end
 
   def get_user_chipcount
@@ -158,18 +147,20 @@ class Blackjack
       puts
       puts "How many chips do you want to play with?"
       puts "Number must be at least 100, multiple of 10"
-      count = gets.chomp().to_i
-    end until (num_at_least_100?(count) & num_is_mult_of_10?(count))
+      count = gets.chomp.to_i
+    end until (number_at_least_100?(count) && number_is_multiple_of_10?(count))
     count
   end
 
   def take_bet
+    player.chip_count < max_bet ? upper_limit = player.chip_count : upper_limit = max_bet
+
     begin
       puts
       puts "How much do you want to bet?"
-      puts "Number must between #{min_bet} and #{max_bet}, and is multiple of 10"
-      bet = gets.chomp().to_i
-    end until (num_is_mult_of_10?(bet) & (bet < (max_bet + 1)) & (bet  > (min_bet - 1)))
+      puts "Number must between #{min_bet} and #{upper_limit}, and is multiple of 10"
+      bet = gets.chomp.to_i
+    end until (number_is_multiple_of_10?(bet) && (bet < (upper_limit + 1)) && (bet  > (min_bet - 1)))
     @player_bet = bet
   end
 
@@ -199,14 +190,66 @@ class Blackjack
     user_move
   end
 
+  def finish_player_hand
+    begin
+      move = take_player_action
+      case move
+      when "H"
+        player.add_card(deck.deal_card)
+        show_dealer_hidden
+      when "S"
+        next
+      else
+        puts "ERROR in Player action loop."
+        exit
+      end
+    end until move == "S"|| player.is_busted?
+  end
+
   def game_reset
     @deck = Deck.new()
     player.cards = []
     dealer.cards = []
   end
 
-  def new_game
+  def dealer_finish_hand
+    if dealer.get_hand_value.last < 17
+      begin
+        show_all_cards
+        dealer.add_card(deck.deal_card)
+        sleep 0.5
+      end until dealer.get_hand_value.last > 16
+    end
+  end
 
+  def player_wins
+    show_all_cards
+    puts "You have #{player.get_hand_value.last} and dealer has #{dealer.get_hand_value.last}."
+    puts "You win $#{player_bet} for this hand"
+    player.chip_count += player_bet
+    dealer.chip_count -= player_bet
+  end
+
+  def player_loses
+    show_all_cards
+    puts "You have #{player.get_hand_value.last} and dealer has #{dealer.get_hand_value.last}."
+    puts "You lose $#{player_bet} for this hand"
+    player.chip_count -= player_bet
+    dealer.chip_count += player_bet
+  end
+
+  def player_out_of_chips?
+    if player.chip_count < min_bet
+      puts "Sorry, you are out of money!  Good bye!"
+      exit
+    end
+  end
+
+  def natural_21_check
+
+  end
+
+  def new_game
     begin
       game_reset
       #need to add play again loop
@@ -224,72 +267,32 @@ class Blackjack
         show_all_cards
         if player.is_natural_21?
           puts "Dealer has Blackjack.  #{player.name} also has Blackjack.  You Tie!"
-          #skips to "do you want to play again??"
         else
           puts "Dealer has Blackjack. You lose this hand."
-          player.chip_count -= player_bet
-          dealer.chip_count += player_bet
-          puts "You lost $#{player_bet} for this hand"
-          #skips to "do you want to play again??"
+          player_loses
         end
       elsif player.is_natural_21?
         show_all_cards
         puts "You have a Blackjack.  You win this hand!"
-        player.chip_count += player_bet
-        dealer.chip_count -= player_bet
+        player.chip_count += player_bet * 1.5
+        dealer.chip_count -= player_bet * 1.5
         puts "You won $#{player_bet} for this hand"
-        #skips to "do you want to play again??"
+      else
+        finish_player_hand
       end
-
-      begin
-        move = take_player_action
-        case move
-        when "H"
-          player.add_card(deck.deal_card)
-          show_dealer_hidden
-        when "S"
-          next
-        else
-          puts "ERROR in Player action loop."
-          exit
-        end
-      end until move == "S"|| player.is_busted?
 
       if player.is_busted?
-        puts "You have busted. You lose this hand!"
-        player.chip_count -= player_bet
-        dealer.chip_count += player_bet
-        puts "You lost $#{player_bet} for this hand"
-        show_all_cards
+        player_loses
       else
-          if dealer.get_count.first < 17
-            begin
-              show_all_cards
-              dealer.add_card(deck.deal_card)
-              sleep 0.5
-            end until dealer.get_count.first > 16
-          end
-
-          if dealer.is_busted?
-            show_all_cards
-            puts "Dealer has busted. You win this hand!"
-            puts "You won $#{player_bet} for this hand"
-            player.chip_count += player_bet
-            dealer.chip_count -= player_bet
-          elsif player.get_count.last > dealer.get_count.last
-            show_all_cards
-            puts "You have #{player.get_count} and dealer has #{dealer.get_count}."
-            puts "You win $#{player_bet} for this hand"
-            player.chip_count += player_bet
-            dealer.chip_count -= player_bet
-          elsif player.get_count.last < dealer.get_count.last
-            show_all_cards
-            puts "You have #{player.get_count} and dealer has #{dealer.get_count}."
-            puts "You lose $#{player_bet} for this hand"
-            player.chip_count -= player_bet
-            dealer.chip_count += player_bet
-          end
+        dealer_finish_hand
+        if dealer.is_busted? || (player.get_hand_value.last > dealer.get_hand_value.last)
+          player_wins
+        else
+          player_loses
+        end
       end
+
+      player_out_of_chips?
 
       puts "Your current chip count: $#{player.chip_count}"
       puts "Do you want to play more hands? y/n"
